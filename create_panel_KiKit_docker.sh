bc26bcbe62ade4555c33ecaeb42ca65e90858edf
Goran Mahovlic <goran.mahovlic@gmail.com>
2025-04-09 15:11:29 +0200

    switching to latest KiKit

--- create_panel_KiKit_docker.sh
+++ create_panel_KiKit_docker.sh
@@ -1,4 +1,4 @@
-docker run -v /home/goran/projects/ulx4m-ls-github:/kikit yaqwsx/kikit:v1.6.0-v8 panelize \
+docker run -v /home/goran/projects/ulx4m-ls-github:/kikit yaqwsx/kikit:latest panelize \
                 --source    'tolerance: 20mm;'                    \
                 --layout    'grid; rows: 4; cols: 4; space: 8mm;' \
                 --framing   'tightframe; width: 5mm; space: 2mm;' \
@@ -9,4 +9,4 @@ docker run -v /home/goran/projects/ulx4m-ls-github:/kikit yaqwsx/kikit:v1.6.0-v8
                 --fiducials '3fid; hoffset: 15mm; voffset: 3mm; coppersize: 2mm; opening: 2.5mm;' \
                 --tooling   '4hole; hoffset: 3mm; voffset: 3mm; size: 3.2mm;' \
                 --text      'simple; text: Intergalaktik d.o.o. ULX4M-LS-v004; anchor: mt; voffset: 3mm; hjustify: center; vjustify: center;' \
-/home/goran/projects/ulx4m-ls-github/ulx4m.kicad_pcb /home/goran/projects/ulx4m-ls-github/panel/ulx4m_panel.kicad_pcb
+ulx4m.kicad_pcb panel/ulx4m_panel.kicad_pcb
